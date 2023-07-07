61f98cec754e5930eb2a856e1a3526a34d7bf874
Amin_Foroutan <aminforoutan7@gmail.com>
2023-07-08 02:48:29 +0330

    [fix]

--- src/pages/Panel/Costs/index.jsx
+++ src/pages/Panel/Costs/index.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom"
 import { CostList } from './List'
 import { AddNewCostPage } from './add'
+import { EditCostPage } from './edit'
+
 
 export const CostsPage = () => {
   return (
@@ -8,6 +10,7 @@ export const CostsPage = () => {
       <Routes>
         <Route path="/" element={<CostList />} />
         <Route path="/add" element={<AddNewCostPage />} />
+        <Route path="/:costID" element={<EditCostPage />} />
       </Routes>
     </div>
   )
